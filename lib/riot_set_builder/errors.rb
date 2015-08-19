b3dcd0a691bc808d522cf1fe250e63edb6b24c2d
Luke Jones <luke@freerunningtechnologies.com>
2015-08-19 10:11:15 -0700

    Add custom errors for builds

---
+++ lib/riot_set_builder/errors.rb
@@ -0,0 +1,10 @@
+module RiotSetBuilder
+  module Error
+    module Build
+      class NoTitle < StandardError; end
+      class InvalidType < StandardError; end
+      class InvalidMap < StandardError; end
+      class InvalidMode < StandardError; end
+    end
+  end
+end
