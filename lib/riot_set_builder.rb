895e238bc5b874ed97f298ed6bea276ed1e7fe32
Luke Jones <luke@freerunningtechnologies.com>
2015-08-24 21:56:48 -0700

    Add configuration to application
    This commit adds a configuration system to our application

--- lib/riot_set_builder.rb
+++ lib/riot_set_builder.rb
@@ -1,6 +1,9 @@
 require "riot_set_builder/version"
+require "riot_set_builder/configuration"
 
 module RiotSetBuilder
+  extend Configuration
+
   def self.generate_builds
     #pro_builds = ProBuilds::Parser.recent_completed_builds.first(3)
     #BuildFile.new(LEAGUE_PATH)
